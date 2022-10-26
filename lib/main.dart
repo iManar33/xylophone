652af4ed29725e209a4f40fe010db8e4e1c710aa
iManar33 <imanarwhynot33@gmail.com>
2022-10-26 20:52:18 +0300

    initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+            child: Center(
+              child: TextButton(onPressed: (){
+                final player = AudioCache();
+                // player.play('assets_note1.wav');
+              }, child: Text('Click ME')),
+            )
+        ),
+      ),
+    );
+  }
+}
